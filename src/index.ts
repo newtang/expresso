c29620ff793639166f27cb194f25a890bc8fd4d2
Jonathan New <jonnew@gmail.com>
2020-06-05 23:50:19 -0700

    Getting some eslint rules set up

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
+import { NextHandleFunction } from 'connect';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function buildRouter(/*options*/): any {
 
-export default function buildRouter(/*options*/){
+	const routes: {[key: string]: {[key: string]: Array<NextHandleFunction>}} = {};
 
-	const routes: {[key:string]: {[key:string]: Array<Function>}} = {};
-
-	const handler = (req, res, next) => {
+	const handler = (req, res, next): void => {
 		routes[req.path].get[0](req, res, next);
-	}
+	};
 	const routerObj = {
-		get: function(path:string, ...handlers:Array<Function>){
+		get: function(path: string, ...handlers: Array<NextHandleFunction>): void {
 			routes[path] = {
 				get: handlers
-			}
+			};
 		}
-	}
+	};
 	return Object.assign(handler, routerObj);
 }
--- src/test/tester.ts
+++ src/test/tester.ts
@@ -7,12 +7,12 @@ const router = expresso();
 const app = express();
 
 // define the home page route
-router.get('/test', function (req, res) {
-  res.send('Birds home page')
-})
+router.get('/test', function(req, res) {
+	res.send('Birds home page');
+});
 
 app.use(router);
 
 const port = 3000;
 app.listen(port);
-console.log(`server listening on ${port}`)
+console.log(`server listening on ${port}`);
