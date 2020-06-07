7bfd7087062cd69169df9bc7fd278e84e94e553f
Jonathan New <jonnew@gmail.com>
2020-06-07 14:34:38 -0700

    Adding support for all http.METHODS. Includes test

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,40 @@
 import { NextHandleFunction } from 'connect';
 import type { Request, Response, NextFunction } from 'express';
 
+import { METHODS } from 'http';
+
+// function getCurrentNodeMethods () {
+//   return http.METHODS && http.METHODS.map(function lowerCaseMethod (method) {
+//     return method.toLowerCase()
+//   })
+// }
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function buildRouter(/*options*/): any {
-
 	const routes: {[key: string]: {[key: string]: Array<NextHandleFunction>}} = {};
 
 	const handler = (req: Request, res: Response, next: NextFunction): void => {
-		routes[req.path].get[0](req, res, next);
+		const verb = req.method.toLowerCase();
+		routes[req.path][verb][0](req, res, next);
 	};
-	const routerObj = {
-		get: function(path: string, ...handlers: Array<NextHandleFunction>): void {
-			routes[path] = {
-				get: handlers
-			};
-		}
-	};
+	
+	const routerObj = buildRouterMethods(routes);
 	return Object.assign(handler, routerObj);
+}
+
+function buildRouterMethods(
+	routes: {[key: string]: {[key: string]: Array<NextHandleFunction>}}): 
+	{[key: string]: (path: string, ...handlers: Array<NextHandleFunction>)=> void}
+{
+	const routerObj: {[key: string]: 
+	(path: string, ...handlers: Array<NextHandleFunction>)=> void;} = {};
+	for(const capsMethod of METHODS){
+		const method = capsMethod.toLowerCase();
+		routerObj[method] = function(path: string, ...handlers: Array<NextHandleFunction>): void {
+			routes[path] = {
+				[method]: handlers
+			};
+		};
+	}
+	return routerObj;
 }
--- src/test/basic.test.ts
+++ src/test/basic.test.ts
@@ -1,7 +1,9 @@
-import request from 'supertest';
+import request, { SuperTest, Test } from 'supertest';
 import express from 'express';
 import expresso from '../index';
 import type { Request, Response } from 'express';
+
+import { METHODS } from 'http';
 
 test('getting started', async () => {
 	const app = express();
@@ -14,4 +16,26 @@ test('getting started', async () => {
 	const res = await request(app).get('/test');
 	expect(res.text).toBe(msg);
 	expect(res.status).toBe(200);
+});
+
+test.each(METHODS)('all methods %s', async (capsMethod) => {
+	if(capsMethod === 'CONNECT'){
+		return;
+	}
+	const app = express();
+	const router = expresso();
+	const msg = 'success';
+	
+	const method = capsMethod.toLowerCase();
+	router[method]('/test', (req: Request, res: Response) => res.send(msg));
+	app.use(router);
+
+	// eslint-disable-next-line @typescript-eslint/ban-types
+	const res = await ((request(app)[method as keyof SuperTest<Test>]) as Function)('/test');
+	if(method !== 'head'){
+		expect(res.text).toBe(msg);
+	}
+	
+	expect(res.status).toBe(200);
+	
 });
