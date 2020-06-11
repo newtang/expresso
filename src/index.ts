bb7655a0719589f6749963470a0603a307ed8985
Jonathan New <jonnew@gmail.com>
2020-06-11 00:10:23 -0700

    Some refactoring to clean up index

--- src/index.ts
+++ src/index.ts
@@ -11,50 +11,47 @@ interface RouterOptions {
 	strict: boolean;
 }
 
+interface RouteMap {
+	[key: string]: {[key: string]: Array<NextHandleFunction>};
+}
+
 const defaultOptions: RouterOptions = {
 	strict: false
 };
 
-// function getCurrentNodeMethods () {
-//   return http.METHODS && http.METHODS.map(function lowerCaseMethod (method) {
-//     return method.toLowerCase()
-//   })
-// }
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function buildRouter(userOptions?: RouterUserOptions): any {
 	const options = Object.assign({}, defaultOptions, userOptions);
-	const routes: {[key: string]: {[key: string]: Array<NextHandleFunction>}} = {};
+	const routes: RouteMap = {};
+	const routerObj = buildRouterMethods(routes, options);
+	return Object.assign(handleRequest.bind(null, routes), routerObj);
+}
 
-	const handler = (req: Request, res: Response, next: NextFunction): void => {
-		const verb = req.method.toLowerCase();
-		let err = false;
-		const pathRoutes = routes[req.path];
-		if(pathRoutes){
-			const verbRoutes = pathRoutes[verb];
-			if(verbRoutes && verbRoutes[0]){
-				verbRoutes[0](req, res, next);
-			}
-			else{
-				err = true;
-			}
+function handleRequest(routes: RouteMap, req: Request, res: Response, next: NextFunction): void {
+	const verb = req.method.toLowerCase();
+	let err = false;
+	const pathRoutes = routes[req.path];
+	if(pathRoutes){
+		const verbRoutes = pathRoutes[verb];
+		if(verbRoutes && verbRoutes[0]){
+			verbRoutes[0](req, res, next);
 		}
 		else{
 			err = true;
 		}
+	}
+	else{
+		err = true;
+	}
 
-		if(err){
-			return next(new Error('404'));
-		}
+	if(err){
+		return next(new Error('404'));
+	}
 
-	};
-	
-	const routerObj = buildRouterMethods(routes, options);
-	return Object.assign(handler, routerObj);
-}
+} 
 
 function buildRouterMethods(
-	routes: {[key: string]: {[key: string]: Array<NextHandleFunction>}},
+	routes: RouteMap,
 	options: RouterOptions): 
 	{[key: string]: (path: string, ...handlers: Array<NextHandleFunction>)=> void}
 	
@@ -63,19 +60,22 @@ function buildRouterMethods(
 	(path: string, ...handlers: Array<NextHandleFunction>)=> void;} = {};
 	for(const capsMethod of METHODS){
 		const method = capsMethod.toLowerCase();
-		routerObj[method] = function(path: string, ...handlers: Array<NextHandleFunction>): void {
-			const validPaths = options.strict
-				? [path]
-				: getNonStrictPaths(path);
-
-			for(const p of validPaths){
-				routes[p] = {
-					[method]: handlers
-				};
-			}
-		};
+		routerObj[method] = addRoute.bind(null, method, routes, options);
 	}
 	return routerObj;
+}
+
+function addRoute(method: string, routes: RouteMap, options: RouterOptions, 
+	path: string, ...handlers: Array<NextHandleFunction>): void {
+	const validPaths = options.strict
+		? [path]
+		: getNonStrictPaths(path);
+
+	for(const p of validPaths){
+		routes[p] = {
+			[method]: handlers
+		};
+	}
 }
 
 function getNonStrictPaths(path: string): Array<string>{
