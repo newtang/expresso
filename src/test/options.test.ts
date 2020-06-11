8e7b3499977723b8cee4d2fe42beff74c5477136
Jonathan New <jonnew@gmail.com>
2020-06-11 00:18:07 -0700

    Testing 'slash' route. Refactor of default test

--- src/test/options.test.ts
+++ src/test/options.test.ts
@@ -4,34 +4,21 @@ import expresso from '../index';
 import type { Request, Response } from 'express';
 
 describe('options tests', () => {
-	test('default strict', async () => {
+	test.each([{strict:false}, undefined])('strict:false', async (options) => {
 		const app = express();
-		const router = expresso();
-		const msg = 'success';
-
-		router.get('/test', (req: Request, res: Response) => res.send(msg));
-		app.use(router);
-
-		const res = await request(app).get('/test');
-		expect(res.text).toBe(msg);
-		expect(res.status).toBe(200);
-
-		const resTrailingSlash = await request(app).get('/test/');
-		expect(resTrailingSlash.text).toBe(msg);
-		expect(resTrailingSlash.status).toBe(200);
-	});
-
-	test('strict:false', async () => {
-		const app = express();
-		const router = expresso({ strict:false });
+		const router = expresso(options);
 		const msg = 'success';
 		const otherMsg = 'alsoSuccess';
+		const slashSuccess = 'slashSuccess';
 
 		//starts with trailing slash
 		router.get('/test', (req: Request, res: Response) => res.send(msg));
 
 		//starts without trailing slash
 		router.get('/othertest/', (req: Request, res: Response) => res.send(otherMsg));
+
+		//base route
+		router.get('/', (req: Request, res: Response) => res.send(slashSuccess));
 		app.use(router);
 
 		const res = await request(app).get('/test');
@@ -50,6 +37,11 @@ describe('options tests', () => {
 		expect(otherResTrailingSlash.text).toBe(otherMsg);
 		expect(otherResTrailingSlash.status).toBe(200);
 
+
+		const slashRes = await request(app).get('/');
+		expect(slashRes.text).toBe(slashSuccess);
+		expect(slashRes.status).toBe(200);
+
 	});
 
 	test('strict:true', async () => {
@@ -57,12 +49,16 @@ describe('options tests', () => {
 		const router = expresso({ strict: true });
 		const msg = 'success';
 		const otherMsg = 'alsoSuccess';
+		const slashSuccess = 'slashSuccess';
 
 		//starts with trailing slash
 		router.get('/test', (req: Request, res: Response) => res.send(msg));
 
 		//starts without trailing slash
 		router.get('/othertest/', (req: Request, res: Response) => res.send(otherMsg));
+
+		//base route
+		router.get('/', (req: Request, res: Response) => res.send(slashSuccess));
 
 		app.use(router);
 
@@ -79,6 +75,10 @@ describe('options tests', () => {
 		const otherResTrailingSlash = await request(app).get('/othertest/');
 		expect(otherResTrailingSlash.text).toBe(otherMsg);
 		expect(otherResTrailingSlash.status).toBe(200);
+
+		const slashRes = await request(app).get('/');
+		expect(slashRes.text).toBe(slashSuccess);
+		expect(slashRes.status).toBe(200);
 	});
 
 });
