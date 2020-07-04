e3d9130dd2bd3a73d8111d8f3ae28de48cb7a575
Jonathan New <jonnew@gmail.com>
2020-07-03 17:28:34 -0700

    Getting basic search functionality working

--- src/paramRadixTreeStorage.ts
+++ src/paramRadixTreeStorage.ts
@@ -1,29 +1,62 @@
 
-export default class Node {
-	edges:Map<string, Node>;
-	param?:string;
-	constructor(path?:string) {
+export default class Node<T> {
+	edges:Map<string, Node<T>>;
+	paramNames?:Array<string>;
+	payload?:T;
+	constructor() {
 		this.edges = new Map();
-		if(path){
-			this.insert(path);
-		}
 	}
 	
-	// search(originalPath:string):{
-	// 	let path = originalPath;
-	// 	let matchIndex;
-	// 	while(path && !this.edges.has(path)){
-	// 		matchIndex = path.lastIndexOf('/') + 1
-	// 		path = path.slice(0, matchIndex);
-	// 	}
-	// 	if(path){
-	// 		const [rest] = originalPath
-	// 		return this.edges.get(path).search(originalPath.slice(matchIndex));
-	// 	}
-	// }
-	insert(path:string){
-		const paramIndex = path.indexOf(':');
+	search(path:string): T | false{
 
+		let currentNode: Node<T> = this;
+		walk:
+		while(true){
+			console.log("search path", path);
+			if(!path){
+				if(currentNode.payload){
+					return currentNode.payload;
+				}
+				else{
+					return false;
+				}
+			}
+			for(const [key, node] of currentNode.edges){
+				if(key !== ':' && path.startsWith(key)){
+					currentNode = node;
+					path = path.slice(key.length);
+					continue walk;
+				}
+			}
+			const paramNode = currentNode.edges.get(':');
+			if(paramNode){
+				currentNode = paramNode;
+				//prevents matching with a starting slash
+				const sliceIndex = path.indexOf('/', 1);
+				if(sliceIndex === -1){
+					path = "";
+				}
+				else{
+					path = path.slice(sliceIndex);
+				}
+				
+			}
+			else{
+				return false;
+			}
+
+
+		}
+	}
+	insert(path:string, payload:T, paramNames:Array<string> = []): void {
+		
+		if(!path){
+			this.payload = payload;
+			this.paramNames = paramNames;
+			return;
+		}
+
+		const paramIndex = path.indexOf(':');
 		let prefix = paramIndex === -1
 			? path
 			: path.slice(0, paramIndex);
@@ -39,13 +72,9 @@ export default class Node {
 				? path.length
 				: nextSlash;
 
-			this.param = path.slice(1, paramEndIndex)
+			paramNames.push(path.slice(1, paramEndIndex));
 			suffix = path.slice(paramEndIndex);
 		}
-
-		console.log("prefix:", prefix);
-		console.log("suffix:", suffix);
-
 
 		//consider leaving colon in, and the constructor determines if it's building a 
 		// a param edge (if the first char is a ':')
@@ -58,7 +87,7 @@ export default class Node {
 
 		if(this.edges.size){
 			if(this.edges.has(prefix)){
-				(this.edges.get(prefix) as Node).insert(suffix);
+				(this.edges.get(prefix) as Node<T>).insert(suffix, payload, paramNames);
 			}
 			else{
 				// const [commonPrefix, otherNode] = longestCommonPrefix(prefix, this.edges);
@@ -66,7 +95,9 @@ export default class Node {
 
 				// }
 				// else{
-					this.edges.set(prefix, new Node(suffix));
+					const newNode = new Node<T>();
+					newNode.insert(suffix, payload, paramNames);
+					this.edges.set(prefix, newNode);
 				// }
 			}
 		}
@@ -77,8 +108,9 @@ export default class Node {
 				
 
 
-
-			this.edges.set(prefix, new Node(suffix));
+			const newNode = new Node<T>();
+			newNode.insert(suffix, payload, paramNames);
+			this.edges.set(prefix, newNode);
 
 			
 		}
--- src/test/paramRadixTreeStorage.test.ts
+++ src/test/paramRadixTreeStorage.test.ts
@@ -7,17 +7,22 @@ describe('param radix tree storage tests', () => {
 	});
 
 	test('insert', () => {
-		const node = new ParamRadixTreeStorage();
-		node.insert("/v1/api/users/:userId/settings");
+		const node = new ParamRadixTreeStorage<string>();
+		const payload = "jackpot!";
+		node.insert("/v1/api/users/:userId/settings", payload);
 		console.log(JSON.stringify(stringify(node), null, 2));
 
-		expect(true).toBe(true);
+		const result = node.search("/v1/api/users/coolUserId/settings");
+		console.log("RESULT", result);
+
+
+		expect(result).toBe(payload);
 	});
 });
 
-function stringify(node: ParamRadixTreeStorage){
+function stringify(node: ParamRadixTreeStorage<string>){
 	const obj:{[key:string]:any} = {};
-	for(const [k,v] of Array.from(node.edges as Map<string, ParamRadixTreeStorage>)){
+	for(const [k,v] of Array.from(node.edges as Map<string, ParamRadixTreeStorage<string>>)){
 		obj[k] = stringify(v); 
 	}
 	return obj;
