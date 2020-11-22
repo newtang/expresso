cfabf3c3dd21bc4b6ccff6012a17431e500adb08
Jonathan New <jonnew@gmail.com>
2020-11-21 16:07:03 -0800

    Added more tests

--- src/test/regex.test.ts
+++ src/test/regex.test.ts
@@ -27,9 +27,63 @@ describe('regex tests', () => {
     expect(resWithError.status).toBe(404);
   });
 
+  test('same regex - different verbs', async () => {
+    const app = express();
+    const router = expresso();
+
+
+    router.get(/^\/test\/$/, (req: Request, res: Response) => res.send("getSuccess"));
+    router.post(/^\/test\/$/, (req: Request, res: Response) => res.send("postSuccess"));
+    app.use(router);
+
+    let res = await request(app).get('/test/');
+    expect(res.text).toBe('getSuccess');
+    expect(res.status).toBe(200);
+
+    res = await request(app).post('/test/');
+    expect(res.text).toBe('postSuccess');
+    expect(res.status).toBe(200);
+
+    res = await request(app).delete('/test/');
+    expect(res.status).toBe(404);
+  });
+
+  test('similar regex - different verbs', async () => {
+    const app = express();
+    const router = expresso();
+
+
+    router.get(/^\/tests?\/$/, (req: Request, res: Response) => res.send("getSuccess"));
+    router.post(/^\/tests\/$/, (req: Request, res: Response) => res.send("postSuccess"));
+    app.use(router);
+
+    let res = await request(app).get('/test/');
+    expect(res.text).toBe('getSuccess');
+    expect(res.status).toBe(200);
+
+    res = await request(app).post('/test/');
+    expect(res.status).toBe(404);
+
+    res = await request(app).get('/tests/');
+    expect(res.text).toBe('getSuccess');
+    expect(res.status).toBe(200);
+
+    res = await request(app).post('/tests/');
+    expect(res.text).toBe('postSuccess');
+    expect(res.status).toBe(200);
+
+    res = await request(app).delete('/test/');
+    expect(res.status).toBe(404);
+  });
+
+
+
   /**
-   * Same regexes but different verbs
+   * x Same regexes but different verbs
    * Similar regexes, but different verbs
+   * combining handlers?
+   * making sure all handlers are called?
+   * order of regexes matter
   **/
 
 });
