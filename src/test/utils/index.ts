52f23c4ee8aa11b71017614d3efdb9b0308894cb
Jonathan New <jonnew@gmail.com>
2021-01-03 16:54:34 -0800

    Tweaking my server utility, removing unneeded library

--- src/test/utils/index.ts
+++ src/test/utils/index.ts
@@ -1,10 +1,12 @@
 import http from 'http';
-import finalhandler from 'finalhandler';
-import type { Server } from 'http';
-import type { NextHandleFunction } from 'connect';
+import type { Server, IncomingMessage, ServerResponse } from 'http';
+import type { NextHandleFunction, NextFunction } from 'connect';
 
 export function createBasicServer(router: NextHandleFunction): Server {
-  return http.createServer(function onRequest(req, res) {
-    router(req, res, finalhandler(req, res));
+  return http.createServer(function onRequest(req: IncomingMessage, res: ServerResponse) {
+    router(req, res, () => {
+		res.statusCode = 404;
+		res.end();
+    });
   });
 }
