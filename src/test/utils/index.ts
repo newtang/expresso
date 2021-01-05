cb3fb4c88b9c32dd0b94392d72d9aebe9f238a00
Jonathan New <jonnew@gmail.com>
2021-01-04 22:18:14 -0800

    Fixing lint issues with test util

--- src/test/utils/index.ts
+++ src/test/utils/index.ts
@@ -1,12 +1,12 @@
 import http from 'http';
 import type { Server, IncomingMessage, ServerResponse } from 'http';
-import type { NextHandleFunction, NextFunction } from 'connect';
+import type { NextHandleFunction } from 'connect';
 
 export function createBasicServer(router: NextHandleFunction): Server {
   return http.createServer(function onRequest(req: IncomingMessage, res: ServerResponse) {
     router(req, res, () => {
-		res.statusCode = 404;
-		res.end();
+      res.statusCode = 404;
+      res.end();
     });
   });
 }
